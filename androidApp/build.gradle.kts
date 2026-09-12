2c6c1f6adb67c422e2eda1e42c4a43d25461b896
Jursin <jursin@126.com>
2026-09-12 13:47:46 +0800

    release: v1.1.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.github.ilife798"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         buildConfigField("String", "API_GATEWAY", "\"${secretsProps.getProperty("API_GATEWAY", "")}\"")
         buildConfigField("String", "SIGN_SALT", "\"${secretsProps.getProperty("SIGN_SALT", "")}\"")
