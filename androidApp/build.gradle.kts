3d90e44b305945ce526ac2ccf04907f7e098d4b2
Jursin <jursin@126.com>
2026-09-14 19:27:41 +0800

    feat: 更新版本号

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "com.github.ilife798"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
 
         buildConfigField("String", "API_GATEWAY", "\"${secretsProps.getProperty("API_GATEWAY", "")}\"")
         buildConfigField("String", "SIGN_SALT", "\"${secretsProps.getProperty("SIGN_SALT", "")}\"")
